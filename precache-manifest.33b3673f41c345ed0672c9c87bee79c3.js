4b081eda0dbb867b8a0096b1ae5187b2a6ad7963
vishwas-yadav-cell <vishwasyadav164@gmail.com>
2020-10-14 22:46:09 +0530

    Updates

---
+++ precache-manifest.33b3673f41c345ed0672c9c87bee79c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060c0a69f9c861c6049b0c16c6722f80",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "919c80de7b4ff117676a",
+    "url": "/React-Todo-App/static/css/main.ea320548.chunk.css"
+  },
+  {
+    "revision": "994b34c083bf40e82d66",
+    "url": "/React-Todo-App/static/js/2.3277913d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo-App/static/js/2.3277913d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "919c80de7b4ff117676a",
+    "url": "/React-Todo-App/static/js/main.8eb8a029.chunk.js"
+  },
+  {
+    "revision": "35169203a1dc3ace5fd5",
+    "url": "/React-Todo-App/static/js/runtime-main.8f6abf7e.js"
+  }
+]);
